cdf77e67784cc1050e3f60ef558617401bbb534c
Vidyasba <116746304+Vidyasba@users.noreply.github.com>
2023-08-18 12:16:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function decimalToBinary(num) {
+function decimalToBinary(decnum) {
   //Write you code here
+	let binary = '';
+    while (decnum > 0) {
+        binary = (decnum % 2) + binary;
+        decnum = Math.floor(decnum / 2);
+    }
+    return binary || '0';
   
 }
 
